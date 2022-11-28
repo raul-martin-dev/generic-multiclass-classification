80ffde6cfeb0d4dd6240fa8f68dea770d8742058
raul-martin-dev <raul.martin4bc@gmail.com>
2022-11-28 19:10:26 +0000

    cleanning implemented and tested

---
+++ source/data/clean.py
@@ -0,0 +1,34 @@
+import pandas as pd
+
+import sys
+sys.path.append("..")
+sys.path.append("../..")
+
+import hydra
+from hydra.core.config_store import ConfigStore
+from config.config import GlobalConfig
+cs = ConfigStore.instance() 
+cs.store(name='nlp_config', node=GlobalConfig)
+
+@hydra.main(version_base=None, config_path='../../config', config_name='config')
+
+def clean(cfg: GlobalConfig):
+    raw_data = pd.read_csv(cfg.paths.raw)
+    df = pd.DataFrame(raw_data)
+    # Get only desired fields
+    clean_data = df.loc[:, [cfg.cleaning.area,cfg.cleaning.description]]
+    # Clean NaN
+    clean_data = clean_data.dropna()
+    # Clean No comments
+    clean_data = clean_data[clean_data[cfg.cleaning.description]!= "No comments"]
+    # Clean duplicate values
+    clean_data = clean_data.drop_duplicates()
+    
+
+    clean_data.to_csv(cfg.paths.clean)
+
+if __name__ == '__main__':
+    clean()
+
+    
+    
